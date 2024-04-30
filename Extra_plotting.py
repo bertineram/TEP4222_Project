c612f30bcd22f93dfbd696ce94c1c204f9e73056
bertineram <bertiner@stud.ntnu.no>
2024-04-30 16:40:49 +0200

    Updating some plots

--- Extra_plotting.py
+++ Extra_plotting.py
@@ -17,24 +17,30 @@ import seaborn as sns
 #%%
 # Picking out the Energy Footprint for the Housing category [MWh]
 EF = pd.read_csv(r"CBi_Urb.csv", header=[0], index_col=[0])
-EF_CP04 = pd.DataFrame(EF.iloc[3, [1,2,3]].values, index=['Cities', 'Towns and suburbs', 'Rural areas'], columns=['CP04'])
-display(EF_CP04)
+EF_CP04 = pd.DataFrame(EF.iloc[3, :].values, index=['CBi', 'EF - Cities', 'EF - Towns and suburbs', 'EF - Rural areas'], columns=['CP04'])
+
+EF_CP04
 
 
-
-EF_CP04.iloc[0,:].values
 
 #%%
 # Import SSB dataset for average price per sqm for the different levels of ubranization
-SSB_sqm_price= pd.read_excel(r"Data/SSB_Sqm_12_22.xlsx", header=0, index_col=[0])
-SSB_sqm_price
+SSB= pd.read_excel(r"Data/SSB_renting_prices_2015.xlsx", header=0, index_col=[0])
+SSB
 
 #%%
 # Ef [MWh] / Average price per sqm [Euro/m2]
-df = pd.DataFrame(index=SSB_sqm_price.index, columns=EF_CP04.index)
-df.loc[2012, :] = EF_CP04.iloc[0,:].values / SSB_sqm_price.loc[2012, :]
-df.loc[2022, :] = EF_CP04.iloc[0,:].values / SSB_sqm_price.loc[2022, :]
+df = pd.DataFrame(index=[''], columns=['Cities', 'Towns and suburbs', 'Rural areas'])
+
+df.loc['', :] = ( EF_CP04.loc['CBi', 'CP04'] * SSB.iloc[0,:] ) / ( SSB.iloc[1,:] )
 df
+
+#%%
+df = pd.DataFrame(index=[''], columns=['Cities', 'Towns and suburbs', 'Rural areas'])
+
+df.loc['', :] = ( EF_CP04.loc[['EF - Cities', 'EF - Towns and suburbs', 'EF - Rural areas'], 'CP04'].values @ SSB.iloc[0,:].values ) / ( SSB.iloc[1,:] ).values
+df
+
 
 #%%###################################################################################
 
@@ -52,32 +58,13 @@ rcParams['font.family'] = 'Times New Roman'
 rcParams['font.size'] = 12
 
 # color nr. 2 is the original color in the first result plot 
-color_palette = ['#23b23c', '#33FF57', '#84ff9a'] 
+# #248b24
+#color_palette = ['#23b23c', '#33FF57', '#84ff9a']
+color_palette = ['#248b24', '#1B681B', '#7FBB7F'] 
+
 
 #%%################################################################
-###                     Vertical bar plot                       ###
-
-rcParams['figure.figsize'] = 8, 4
-
-ax = df.plot(kind = 'bar', color=color_palette)#, color= sns.color_palette('colorblind', n_colors=3))
-
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[::-1], labels[::-1], title='Level of urbanization', bbox_to_anchor=(1, 1), loc=2, borderaxespad=0, fontsize=14)
-
-plt.yticks(np.arange(0, 6, 1.0))
-
-# Rotating X-axis labels
-plt.xticks(rotation = 360)
-
-plt.ylabel('MWh / (Euro / $m^2$)')
-
-plt.show()
-
-#plt.xlabel('---')
-#plt.title('', fontsize=12)
-
-#%%################################################################
-###                     Vertical bar plot                       ###
+###                    Horizontal bar plot                      ###
 sns.set_style("whitegrid")
 
 plt.rcParams['figure.figsize'] = 8, 4
@@ -89,24 +76,48 @@ n_bars = df.shape[0]
 bar_width = 0.5
 
 # Plotting the bars
-ax = df.plot(kind='bar', color=color_palette, width=bar_width)
+ax = df.plot(kind='barh', color=color_palette, width=bar_width)
 
-# Remove vertical grid lines
-ax.grid(axis='x', linestyle='-')
 
 # Moving the legend to below the plot
 handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.15), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
+ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.25), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
 
 # Adjusting y-ticks
-plt.yticks(np.arange(0, 6, 1.0))
-
+#plt.yticks(np.arange(0, 6, 1.0))
 # Rotating X-axis labels
 plt.xticks(rotation=360)
 
-plt.ylabel('MWh / (Euro / $m^2$)')
 
-plt.tight_layout()  # Adjust layout to prevent clipping of labels
+plt.xlabel('$MWh / m^2$')
+
+# Adjust layout to prevent clipping of labels and adjust spacing
+plt.tight_layout()
 
 plt.show()
 
+
+#%%##################################################################
+
+plt.rcParams['figure.figsize'] = 8, 4
+
+# Width of each bar
+bar_width = 0.5
+
+# Plotting the bars
+ax = df.plot(kind='bar', color=color_palette, width=bar_width)
+
+
+# Moving the legend to below the plot
+#handles, labels = ax.get_legend_handles_labels()
+#ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.25), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
+
+# Adjusting y-ticks
+#plt.yticks(np.arange(0, 6, 1.0))
+
+plt.ylabel('$MWh / m^2$')
+
+# Adjust layout to prevent clipping of labels and adjust spacing
+plt.tight_layout()
+
+plt.show()
