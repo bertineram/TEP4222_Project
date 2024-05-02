d983ff1180b85402f3ab7c2644f9b01a80c73d7e
bertineram <bertiner@stud.ntnu.no>
2024-05-02 22:42:24 +0200

    nothing special

--- Extra_plotting.py
+++ Extra_plotting.py
@@ -48,18 +48,12 @@ df
 
 ######################################################################################
 
-# Energy Footprint / (Average price per m2) [MWh/(Euro/m2)]
-
-        # print(sns.color_palette("pastel6").as_hex())     # see colors as hex for indvidual plots
-
 # Setting the figure size and font adjustments
 rcParams['figure.figsize'] = 8, 10 # Inches
 rcParams['font.family'] = 'Times New Roman'
 rcParams['font.size'] = 12
 
-# color nr. 2 is the original color in the first result plot 
-# #248b24
-#color_palette = ['#23b23c', '#33FF57', '#84ff9a']
+# color #248b24 is the original color in the first result plot for Energy Footprint
 color_palette = ['#248b24', '#1B681B', '#7FBB7F'] 
 
 
@@ -69,55 +63,19 @@ sns.set_style("whitegrid")
 
 plt.rcParams['figure.figsize'] = 8, 4
 
-# Determine the number of bars
-n_bars = df.shape[0]
-
-# Width of each bar
-bar_width = 0.5
 
 # Plotting the bars
-ax = df.plot(kind='barh', color=color_palette, width=bar_width)
-
+plt.barh(df.columns, df.iloc[0,:], height=0.5, color=['#248b24', '#1B681B', '#7FBB7F'])
 
 # Moving the legend to below the plot
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.25), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
-
-# Adjusting y-ticks
-#plt.yticks(np.arange(0, 6, 1.0))
-# Rotating X-axis labels
-plt.xticks(rotation=360)
-
+#handles, labels = ax.get_legend_handles_labels()
+#ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.25), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
 
 plt.xlabel('$MWh / m^2$')
 
 # Adjust layout to prevent clipping of labels and adjust spacing
 plt.tight_layout()
 
-plt.show()
-
-
-#%%##################################################################
-
-plt.rcParams['figure.figsize'] = 8, 4
-
-# Width of each bar
-bar_width = 0.5
-
-# Plotting the bars
-ax = df.plot(kind='bar', color=color_palette, width=bar_width)
-
-
-# Moving the legend to below the plot
-#handles, labels = ax.get_legend_handles_labels()
-#ax.legend(handles, labels, bbox_to_anchor=(0.5, -0.25), loc='upper center', borderaxespad=0, fontsize=12, ncol=len(labels))
-
-# Adjusting y-ticks
-#plt.yticks(np.arange(0, 6, 1.0))
-
-plt.ylabel('$MWh / m^2$')
-
-# Adjust layout to prevent clipping of labels and adjust spacing
-plt.tight_layout()
 
 plt.show()
+
